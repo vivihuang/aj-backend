c02770ac5a1761716cb1a5a31245db45566d593c
Anddd7 <liaoad_space@sina.com>
2019-11-07 15:13:55 +0800

    respond image version & profile in healthy check endpoint

--- src/main/kotlin/com/askjamie/latvia/hello/HelloController.kt
+++ src/main/kotlin/com/askjamie/latvia/hello/HelloController.kt
@@ -1,12 +1,23 @@
 package com.askjamie.latvia.hello
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloController {
+class HelloController(private val environment: Environment) {
+
+    @Value("\${image.version}")
+    private lateinit var imageVersion: String
 
     @GetMapping("/hello")
     fun hello(@RequestParam(defaultValue = "World") name: String) = "hello, $name!"
+
+    @GetMapping("/profile")
+    fun profile() = mapOf(
+            "build.env" to environment.activeProfiles,
+            "image.version" to imageVersion
+    )
 }
